74740c44eeba4ac3aaed74f20b5f8870053bd860
akbar gulyamov <kbrgulyamov@gmail.com>
2023-02-12 20:31:28 +0500

    feat:  insaet auth [nextauth] add JWT_SECRET login page add

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,6 +6,13 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
     })
   ],
   secret: process.env.JWT_SECRET
